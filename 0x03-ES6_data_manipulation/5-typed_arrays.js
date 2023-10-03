cf8c303935c318fed7498be20316dba0eb9e7ab4
‘Shane-dev-create’ <shn.shane@gmail.com>
2023-10-03 11:17:37 -0700

    Retry Task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+
   const buffer = new ArrayBuffer(length);
-  const dv = new DataView(buffer);
-
-  // check if position outside range
-  if (position >= length) throw new Error('Position outside range');
-  // set the new value in the desired position
-  dv.setInt8(position, value);
-
-  return dv;
-}}
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return view;
+}
