44d7894bc71d58e0e56d73ac98320d00dd9961c9
‘Shane-dev-create’ <shn.shane@gmail.com>
2023-10-03 11:10:17 -0700

    hot fix task 5 and advanced task

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,17 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
-  // check if position is outside of range
+  // check if position is outside ofexport default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const dv = new DataView(buffer);
+
+  // check if position outside range
+  if (position >= length) throw new Error('Position outside range');
+  // set the new value in the desired position
+  dv.setInt8(position, value);
+
+  return dv;
+} range
   if (position >= length) throw new Error('Position outside range');
   // set the new value in the desired position
   dv.setInt8(position, value);
